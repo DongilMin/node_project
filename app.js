b8705b79bf2cb76ed6dbcedd8b107fad80e69004
DongilMin <tasabi0826@gmail.com>
2025-02-22 23:37:34 +0900

    초기 세팅, DB세팅

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+const { sequlize, sequelize } = require('./models');
+
+dotenv.config(); // process.env
+const pageRouter = require('./routes/page');
+
+const app = express();
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+sequelize.sync({force : false})
+    .then(() => {
+        console.log('데이터베이스 연결 성공');
+    })
+
+app.use(morgan('dev')); // 개발모드로 로깅
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+
+    }
+}));
+
+app.use('/', pageRouter);
+app.use((req, res, next) => { // 404 not found
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+app.use((err, req, res, next) => {
+    res.locals.message = urr.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err: {}; // 에러 로그를 서비스한테 넘김
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '빈 포트에서 대기중');
+});
